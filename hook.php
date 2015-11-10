f4365edac9c9e5a50736141eb33b6de12241105e
Jose Luis Poveda <joseluis.poveda@ofertix.com>
2015-11-10 12:48:25 +0100

    Cambiado el hook

--- hook.php
+++ hook.php
@@ -4,7 +4,7 @@ $loader = require __DIR__.'/vendor/autoload.php';
 
 $API_KEY = '146668771:AAE9_zVF-YnDco0RcR-xxycUXeh1Cs15E6o';
 $BOT_NAME = 'OfertixItBot';
-//$COMMANDS_FOLDER = __DIR__.'/Commands/';
+$COMMANDS_FOLDER = __DIR__.'/src/Commands/';
 //$credentials = array('host'=>'localhost', 'user'=>'dbuser', 'password'=>'dbpass', 'database'=>'dbname');
 
 try {
@@ -15,13 +15,13 @@ try {
 
     //$telegram->enableMySQL($credentials);
     //$telegram->enableMySQL($credentials, $BOT_NAME.'_');
-    //$telegram->addCommandsPath($COMMANDS_FOLDER);
+    $telegram->addCommandsPath($COMMANDS_FOLDER);
     // here you can set some command specified parameters,
     // for example, google geocode/timezone api key for date command:
     //$telegram->setCommandConfig('date', array('google_api_key'=>'your_google_api_key_here'));
-    //$telegram->setLogRequests(true);
-    //$telegram->setLogPath($BOT_NAME.'.log');
-    //$telegram->setLogVerbosity(4);
+    $telegram->setLogRequests(true);
+    $telegram->setLogPath($BOT_NAME.'.log');
+    $telegram->setLogVerbosity(4);
 
     //$telegram->setDownloadPath("../Download");
     //$telegram->setUploadPath("../Upload");
